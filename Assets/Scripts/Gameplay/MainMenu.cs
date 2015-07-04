5bfe66553dcf8724393fae8a7b1a0e9db2384078
Ray Frye <rfrye09@gmail.com>
2015-07-04 07:57:35 -0400

    Build 1
    Build Main Menu scene, connect to Shop scene.

---
+++ Assets/Scripts/Gameplay/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour 
+{
+	public void StartGame()
+	{
+		Application.LoadLevel ("Shop");
+	}
+}
